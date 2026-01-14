5a7a4e234009e8b65238e6d1dd476055240060a0
kfirbros123 <bobytb143@gmail.com>
2026-01-14 18:57:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import boto3
 # --- AWS credentials ---
 aws_access_key_id = "access"
 aws_secret_access_key = "secret"
-aws_session_token = "TOKEN"
+aws_session_token = "token"
 region_name = "us-east-1"
+
+# --- AWS variables ---
+vpc="vpc-0fd581580b59f8d29"
+subnet="subnet-07d5e49c3a9d0518c"
+image='ami-0d03b8975f8354317'
+securityGroup='sg-0dca345f4c0814660'
 
 # --- Clients ---
 ec2_client = boto3.client(
@@ -30,20 +36,22 @@ elbv2_client = boto3.client(
 )
 
 # --- Step 1: Create Target Group ---
+tg_name="cpu-target-group"
 tg_response = elbv2_client.create_target_group(
-    Name='new-cpu-burner-tg',
+    Name=tg_name,
     Protocol='TCP',
     Port=80,
-    VpcId='vpc-088019931a2d8797c',  # replace with your VPC ID
+    VpcId=vpc,  # replace with your VPC ID
     TargetType='instance'
 )
 target_group_arn = tg_response['TargetGroups'][0]['TargetGroupArn']
 print("Target Group ARN:", target_group_arn)
 
 # --- Step 2: Create Network Load Balancer ---
+nlb_name="cpu-LB"
 nlb_response = elbv2_client.create_load_balancer(
-    Name='my-nlb',
-    Subnets=['subnet-02a7120cd95210421'],  # replace with your subnets
+    Name=nlb_name,
+    Subnets=[subnet],  # replace with your subnets
     Scheme='internet-facing',
     Type='network',
 )
@@ -64,18 +72,19 @@ listener_arn = listener_response['Listeners'][0]['ListenerArn']
 print("Listener ARN:", listener_arn)
 
 # --- Step 3: Create Launch Template ---
+lt_name='cpu-burner-template'
 launch_template_response = ec2_client.create_launch_template(
-    LaunchTemplateName='cpu-burner-new-template',
+    LaunchTemplateName=lt_name,
     LaunchTemplateData={
-        'ImageId': 'ami-049b59c8203345952',  # replace with your AMI
+        'ImageId': image,  # replace with your AMI
         'InstanceType': 't3.medium',
         'KeyName': 'kfir-key',            # replace with your key pair
         'NetworkInterfaces': [
             {
                 'DeviceIndex': 0,
-                'SubnetId': 'subnet-04b96cd3bf4703ab6',  # replace with your subnet
+                'SubnetId': subnet,  # replace with your subnet
                 'AssociatePublicIpAddress': True,
-                'Groups': ['sg-06f03bef8203f3391']       # your security group(s)
+                'Groups': [securityGroup]       # your security group(s)
             }
         ]
         
@@ -85,7 +94,7 @@ launch_template_id = launch_template_response['LaunchTemplate']['LaunchTemplateI
 print("Launch Template ID:", launch_template_id)
 
 # --- Step 4: Create Auto Scaling Group attached to NLB Target Group ---
-asg_name = "cpu-burner-new-asg"
+asg_name = "cpu-burner-autoscaling"
 
 asg_response = autoscaling_client.create_auto_scaling_group(
     AutoScalingGroupName=asg_name,
@@ -93,14 +102,14 @@ asg_response = autoscaling_client.create_auto_scaling_group(
         'LaunchTemplateId': launch_template_id,
         'Version': '$Latest'
     },
-    MinSize=1,
+    MinSize=2,
     MaxSize=5,
     DesiredCapacity=2,
-    VPCZoneIdentifier='subnet-04b96cd3bf4703ab6',
+    VPCZoneIdentifier=subnet,
     TargetGroupARNs=[target_group_arn],  # attach NLB target group
     Tags=[{
         'Key': 'Name',
-        'Value': 'MyASGInstance',
+        'Value': 'MyASGInstance2',
         'PropagateAtLaunch': True
     }]
 )
@@ -110,17 +119,17 @@ print("Auto Scaling Group created and attached to NLB:", asg_response)
 
 
 # --- Step 4: Create Dynamic Scaling Policy ---
+pol_name = 'cpu-policy'
 policy_response = autoscaling_client.put_scaling_policy(
     AutoScalingGroupName=asg_name,
-    PolicyName='CPUScaleOutPolicy',
+    PolicyName= pol_name,
     PolicyType='TargetTrackingScaling',
     TargetTrackingConfiguration={
         'PredefinedMetricSpecification': {
             'PredefinedMetricType': 'ASGAverageCPUUtilization'
         },
         'TargetValue': 50.0,  # keep average CPU around 50%
-        'DisableScaleIn': False,
-        'EstimatedInstanceWarmup': 120  # 300 seconds = 5 minutes
+        'DisableScaleIn': False
     }
 )
 
